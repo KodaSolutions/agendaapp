c441535a936fd4b23748d951ef2e67cd3512e3a6
AntonioRiosCovian <joserioscovian@gmail.com>
2024-12-18 05:14:09 -0600

    new pin widget created, pin screen unlinked

---
+++ lib/services/authService2.dart
@@ -0,0 +1,80 @@
+import 'dart:convert';
+
+import 'package:agenda_app/views/login.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../globalVar.dart';
+import '../views/admin/admin.dart';
+import 'angedaDatabase/databaseService.dart';
+
+class AuthService2 {
+  final String baseUrl = 'https://agendapp-cvp-75a51cfa88cd.herokuapp.com/api/login';
+
+  Future<void> authenticate(BuildContext context, String userId, bool docLog, String pin, TextEditingController pinController) async {
+    try {
+      String jsonBody = json.encode({
+        'identification': userId,
+        'password': pin,
+        'fcm_token': await FirebaseMessaging.instance.getToken(),
+      });
+
+      var response = await http.post(
+        Uri.parse(baseUrl),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonBody,
+      );
+
+      if (response.statusCode == 200) {
+        var data = json.decode(response.body);
+        final dbService = DatabaseService();
+        await dbService.saveSession(
+            data['token'],
+            data['user']['id'],
+            data['user']['role'] == 'doctor'
+        );
+
+        await dbService.saveUser({
+          'id': data['user']['id'],
+          'name': data['user']['name'],
+          'email': data['user']['email'],
+          'identification': data['user']['identification'],
+          'role': data['user']['role'],
+          'fcm_token': data['user']['fcm_token'],
+        });
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setString('jwt_token', data['token']);
+        await prefs.setInt('user_id', data['user']['id']);
+        print('hola: ${data['user']}');
+        SessionManager.instance.isDoctor = data['user']['role'] == 'doctor';
+        SessionManager.instance.Nombre = data['user']['name'];
+
+        SessionManager.instance.userRole = data['user']['role'];
+
+        if(context.mounted) {
+          Navigator.of(context).pushAndRemoveUntil(
+            CupertinoPageRoute(
+              builder: (context) => AssistantAdmin(
+                docLog: SessionManager.instance.isDoctor,
+              ),
+            ),
+            (Route<dynamic> route) => false,
+          );
+        }
+
+      } else {
+        print("Autenticación fallida: ${response.body}");
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          pinController.clear(); // Limpia el campo de PIN en el hilo principal
+        });
+      }
+    } catch (e) {
+      print("Authentication Error: $e");
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        pinController.clear(); // Limpia el campo de PIN en el hilo principal
+      });
+    }
+  }
+}
--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,10 +1,11 @@
 import 'dart:ui';
 
+import 'package:agenda_app/services/authService2.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import '../projectStyles/appColors.dart';
-import '../services/auth_service.dart';
 import '../styles/ladingDraw.dart';
 
 class Login extends StatefulWidget {
@@ -17,14 +18,17 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   final TextEditingController _identificationController = TextEditingController();
   final FocusNode _identificationFocusNode = FocusNode();
+  final TextEditingController _pinController = TextEditingController();
 
   bool showPinEntryScreen = false;
   String userIdentification = '';
+  bool showPinField = false;
+  final FocusNode _inputFocusNode = FocusNode();
+  final AuthService2 authService = AuthService2();
 
   @override
   void initState() {
     super.initState();
-    // Agregar listener para detectar cambios en el texto
     _identificationController.addListener(_onIdentificationChanged);
   }
 
@@ -33,6 +37,7 @@ class _LoginState extends State<Login> {
     _identificationController.removeListener(_onIdentificationChanged);
     _identificationController.dispose();
     _identificationFocusNode.dispose();
+    _inputFocusNode.dispose();
     super.dispose();
   }
 
@@ -93,80 +98,26 @@ class _LoginState extends State<Login> {
                     ),
                     child: Column(
                       children: [
-                        Container(
-                            decoration: const BoxDecoration(
-                              color: AppColors3.whiteColor,
-                              borderRadius: BorderRadius.all(Radius.circular(10)),
-                            ),
-                            height: MediaQuery.of(context).size.height * 0.065,
-                            margin: EdgeInsets.symmetric(
-                              horizontal: MediaQuery.of(context).size.width * 0.095,
-                            ),
-                            child: Row(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.only(
-                                      left: MediaQuery.of(context).size.width * 0.035, right: MediaQuery.of(context).size.width * 0.035),
-                                  child: SvgPicture.asset(
-                                    'assets/icons/drIcon.svg',
-                                    color: AppColors3.primaryColor,
-                                    width: MediaQuery.of(context).size.width * 0.105,
-                                  ),
+                        AnimatedSwitcher(
+                          duration: const Duration(milliseconds: 300),
+                          transitionBuilder: (Widget child, Animation<double> animation) {
+                            return SlideTransition(
+                              position: Tween<Offset>(
+                                begin: const Offset(1.0, 0.0),
+                                end: Offset.zero,
+                              ).animate(animation),
+                              child: child,
+                            );
+                          },
+                          child: showPinField
+                              ? KeyedSubtree(
+                                  key: const ValueKey('pinField'),
+                                  child: _buildPinField(),
+                                )
+                              : KeyedSubtree(
+                                  key: const ValueKey('identificationField'),
+                                  child: _buildIdentificationField(),
                                 ),
-                                Container(
-                                  margin: EdgeInsets.only(
-                                      left: MediaQuery.of(context).size.width * 0.015,
-                                      right: MediaQuery.of(context).size.width * 0.015),
-                                  height: MediaQuery.of(context).size.width * 0.09,
-                                  width: MediaQuery.of(context).size.width * 0.006,
-                                  decoration: BoxDecoration(
-                                    color: AppColors3.primaryColor,
-                                    border: Border.all(width: 0.5, color: AppColors3.primaryColor),
-                                    borderRadius: BorderRadius.all(Radius.circular(5)),
-                                  ),
-                                ),
-                                Flexible(
-                                  child: TextField(
-                                    controller: _identificationController,
-                                    focusNode: _identificationFocusNode,
-                                    //keyboardType: TextInputType.number,
-                                    inputFormatters: const [
-                                      /*        FilteringTextInputFormatter.digitsOnly,
-                                  LengthLimitingTextInputFormatter(3),*/
-                                    ],
-                                    textAlign: TextAlign.start,
-                                    style: const TextStyle(
-                                      color: AppColors3.primaryColor,
-                                      fontSize: 26,
-                                    ),
-                                    decoration: InputDecoration(
-                                      isDense: true,
-                                      hintText: 'Ingresar identificación...',
-                                      hintStyle: TextStyle(
-                                        color: AppColors3.primaryColor.withOpacity(0.5),
-                                        fontSize: 18,
-                                      ),
-                                      filled: true,
-                                      fillColor: AppColors3.whiteColor,
-                                      enabledBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(10.0),
-                                        borderSide: const BorderSide(
-                                          color: Colors.transparent,
-                                          width: 2,
-                                        ),
-                                      ),
-                                      focusedBorder: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(10.0),
-                                        borderSide: const BorderSide(
-                                          color: Colors.transparent,
-                                          width: 2,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            )
                         ),
                       ],
                     ),
@@ -188,29 +139,211 @@ class _LoginState extends State<Login> {
                 ),
               ],
             ),
-
-            // Pin Entry Screen
-            Visibility(
-              visible: showPinEntryScreen,
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                child: PinEntryScreen(
-                  userId: userIdentification,
-                  docLog: true,
-                  onCloseScreeen: (closeScreen) {
-                    setState(() {
-                      if (closeScreen) {
-                        showPinEntryScreen = false;
-                        _identificationController.clear();
-                      }
-                    });
-                  },
-                ),
-              ),
-            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildIdentificationField() {
+    return Container(
+      key: const ValueKey('identificationField'),
+      decoration: const BoxDecoration(
+        color: AppColors3.whiteColor,
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      height: MediaQuery.of(context).size.height * 0.065,
+      margin: EdgeInsets.symmetric(
+        horizontal: MediaQuery.of(context).size.width * 0.095,
+      ),
+      child: Row(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+              left: MediaQuery.of(context).size.width * 0.035,
+              right: MediaQuery.of(context).size.width * 0.035,
+            ),
+            child: SvgPicture.asset(
+              'assets/icons/drIcon.svg',
+              color: AppColors3.primaryColor,
+              width: MediaQuery.of(context).size.width * 0.105,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              left: MediaQuery.of(context).size.width * 0.015,
+              right: MediaQuery.of(context).size.width * 0.015,
+            ),
+            height: MediaQuery.of(context).size.width * 0.09,
+            width: MediaQuery.of(context).size.width * 0.006,
+            decoration: BoxDecoration(
+              color: AppColors3.primaryColor,
+              border: Border.all(width: 0.5, color: AppColors3.primaryColor),
+              borderRadius: const BorderRadius.all(Radius.circular(5)),
+            ),
+          ),
+          Flexible(
+            child: TextField(
+              controller: _identificationController,
+              focusNode: _inputFocusNode,
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                LengthLimitingTextInputFormatter(3),
+              ],
+              textAlign: TextAlign.start,
+              style: const TextStyle(
+                color: AppColors3.primaryColor,
+                fontSize: 26,
+              ),
+              decoration: InputDecoration(
+                isDense: true,
+                hintText: 'Ingresar identificación...',
+                hintStyle: TextStyle(
+                  color: AppColors3.primaryColor.withOpacity(0.5),
+                  fontSize: 18,
+                ),
+                filled: true,
+                fillColor: AppColors3.whiteColor,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: const BorderSide(
+                    color: Colors.transparent,
+                    width: 2,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: const BorderSide(
+                    color: Colors.transparent,
+                    width: 2,
+                  ),
+                ),
+              ),
+              onChanged: (value) {
+                if (value.length == 3) {
+                  setState(() {
+                    showPinField = true;
+                    FocusScope.of(context).requestFocus(_inputFocusNode);
+                  });
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPinField() {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.065,
+      margin: EdgeInsets.symmetric(
+        horizontal: MediaQuery.of(context).size.width * 0.095,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          IconButton(
+            color: AppColors3.whiteColor,
+            onPressed: () {
+              setState(() {
+                showPinField = false;
+                _identificationController.clear();
+              });
+            },
+            icon: Icon(
+              CupertinoIcons.left_chevron,
+              size: MediaQuery.of(context).size.width * 0.095,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: AppColors3.whiteColor,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              height: MediaQuery.of(context).size.height * 0.065,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                      left: MediaQuery.of(context).size.width * 0.035,
+                      right: MediaQuery.of(context).size.width * 0.035,
+                    ),
+                    child: SvgPicture.asset(
+                      'assets/icons/pinIcon.svg',
+                      color: AppColors3.primaryColor,
+                      width: MediaQuery.of(context).size.width * 0.105,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      left: MediaQuery.of(context).size.width * 0.015,
+                      right: MediaQuery.of(context).size.width * 0.015,
+                    ),
+                    height: MediaQuery.of(context).size.width * 0.09,
+                    width: MediaQuery.of(context).size.width * 0.006,
+                    decoration: BoxDecoration(
+                      color: AppColors3.primaryColor,
+                      border: Border.all(width: 0.5, color: AppColors3.primaryColor),
+                      borderRadius: const BorderRadius.all(Radius.circular(5)),
+                    ),
+                  ),
+                  Flexible(
+                    child: TextField(
+                      focusNode: _inputFocusNode,
+                      controller: _pinController,
+                      keyboardType: TextInputType.number,
+                      obscureText: true, // Oculta el texto ingresado
+                      maxLength: 4,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                        LengthLimitingTextInputFormatter(4),
+                      ],
+                      textAlign: TextAlign.start,
+                      style: const TextStyle(
+                        color: AppColors3.primaryColor,
+                        fontSize: 26,
+                      ),
+                      decoration: InputDecoration(
+                        counterText: '',
+                        isDense: true,
+                        hintText: 'Ingresar PIN...',
+                        hintStyle: TextStyle(
+                          color: AppColors3.primaryColor.withOpacity(0.5),
+                          fontSize: 18,
+                        ),
+                        filled: true,
+                        fillColor: AppColors3.whiteColor,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: const BorderSide(
+                            color: Colors.transparent,
+                            width: 2,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: const BorderSide(
+                            color: Colors.transparent,
+                            width: 2,
+                          ),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        if (value.length == 4) {
+                          print('Autenticando...');
+                          authService.authenticate(context, userIdentification, true, value, _pinController);
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
